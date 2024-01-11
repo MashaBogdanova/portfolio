cf188a66a94773b233b11278609988a286b1e738
Maria Bogdanova <masha.bogdanoff@gmail.com>
2024-01-11 20:31:54 +0100

    feat: add responsive and adaptive styles

--- app/components/MainInfo/Description/Description.js
+++ app/components/MainInfo/Description/Description.js
@@ -6,8 +6,8 @@ function Description() {
     return (
         <article className={styles.description}>
             <Image className={styles.description__photo} src={mainInfo.photo} alt="Maria Bogdanova"/>
-            <div className={styles.description__text}>
-                <h1 className={styles.description__title}>{mainInfo.name}</h1>
+            <div className={styles.description__textBlock}>
+                <h1>{mainInfo.name}</h1>
                 <h4 className={styles.description__subtitle}>{mainInfo.speciality}</h4>
                 <p className={styles.description__summary}>{mainInfo.summary}</p>
             </div>
