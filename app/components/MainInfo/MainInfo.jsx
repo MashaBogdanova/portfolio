e2b76b0bd3d41032fb246aef8b1414b5b43ae7e9
Maria Bogdanova <masha.bogdanoff@gmail.com>
2024-01-12 13:16:34 +0100

    feat: add adaptive and responsive styles for mobile

--- app/components/MainInfo/MainInfo.jsx
+++ app/components/MainInfo/MainInfo.jsx
@@ -1,12 +1,18 @@
 import styles from './main-info.module.css';
 import Description from '/app/components/MainInfo/Description/Description';
 import IconsList from 'app/components/IconsList';
-import { contactsList } from '/app/config/portfolio.config';
+import { contactsList, mainInfo } from '/app/config/portfolio.config';
+import Image from 'next/image';
 
 function MainInfo() {
   return (
     <section className={styles.mainInfo}>
       <div className={styles.mainInfo__content}>
+        <Image
+          className={styles.mainInfo__photo}
+          src={mainInfo.photo}
+          alt="Maria Bogdanova"
+        />
         <Description />
         <IconsList list={contactsList} type='contacts' />
       </div>
